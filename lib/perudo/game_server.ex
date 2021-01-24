e3fd355b0aefd935af4b654f9677a41d6064aa9f
Max Tyler <maxastyler@gmail.com>
2021-01-24 11:05:08 +0000

    change server to statemachine

--- lib/perudo/game_server.ex
+++ lib/perudo/game_server.ex
@@ -2,35 +2,38 @@ defmodule Perudo.GameServer do
   @moduledoc """
   This module implements the GenServer that players connect to to play a game of perudo
   """
-  use GenServer, restart: :transient
+  use GenStateMachine
 
   alias Perudo.PubSub
   alias Perudo.Game
 
   def start_link(options) do
     {state, opts} = Keyword.pop(options, :state)
-    GenServer.start_link(__MODULE__, state, opts)
+    GenStateMachine.start_link(__MODULE__, {:lobby, state}, opts)
   end
 
   @impl true
-  def init(%{room: room, players: players}) do
-    {:ok, %{room: room, players: players, started: false, game: nil}}
+  def init({state, data}) do
+    {:ok, state, data}
   end
 
   @impl true
-  def handle_call({:add_player, player}, _from, %{game: nil} = state),
-    do: {:reply, :added_player, update_in(state[:players], &MapSet.put(&1, player))}
+  def handle_event({:call, from}, {:add_player, player}, :lobby, data) do
+    new_data = update_in(data, [:players], &MapSet.put(&1, player))
+	{:keep_state, new_data, {:reply, from, {:ok, new_data}}}
+  end
 
-  def handle_call({:add_player, _}, _from, state), do: {:reply, :game_started, state}
+  def handle_event({:call, from}, {:remove_player, player}, :lobby, data) do
+    new_data = update_in(data, [:players], &MapSet.delete(&1, player))
+	{:keep_state, new_data, {:reply, from, {:ok, new_data}}}
+  end
 
-  @impl true
-  def handle_call(:start_game, _from, %{game: nil} = state) do
-    if MapSet.size(state[:players]) > 1 do
-      game = Game.new_game(MapSet.to_list(state[:players]))
-      PerudoWeb.Endpoint.broadcast(state[:room], "game_updated", game)
-      {:reply, :started, put_in(state[:game], game)}
-    else
-      {:reply, {:error, "can't start until there are >1 players"}, state}
-    end
+  def handle_event({:call, from}, :start_game, :lobby, data) do
+	game = Perudo.Game.new_game(MapSet.to_list(data.players))
+    {:next_state, :in_game, Map.put(data, :game, game), {:reply, from, "jo"}}
+  end
+
+  def add_player(server, player) do
+    GenStateMachine.call(server, {:add_player, player})
   end
 end
